db63d4617c899796b4d41f05688a6944b8d8ccbf
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-05-19 08:28:53 -0300

    feat: use client component

--- app/product/AddToCart.tsx
+++ app/product/AddToCart.tsx
@@ -1,12 +1,22 @@
+"use client"
+import { useState } from "react";
+
 interface MyComponentProps {
   productId: number;
 }
 
 const AddToCart: React.FC<MyComponentProps> = ({ productId }) => {
+  const [added, setAdded] = useState(false);
+
+  const handleAddToCart = () => {
+    setAdded(true)
+    console.log(`Product ${productId} added to cart.`)
+  }
+
   return (
-    <div>
-      <p>ID: {productId}</p>
-    </div>
+    <button onClick={handleAddToCart}>
+      {added ? "Added to cart" : "Add to cart"}
+    </button>
   );
 };
 
