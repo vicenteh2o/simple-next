f5431cb0ebd6094abf3a0ef245badb0f96d80df4
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-05-19 11:17:00 -0300

    feat: post

--- app/api/todoItem/route.ts
+++ app/api/todoItem/route.ts
@@ -1,8 +1,25 @@
 import { NextResponse } from "next/server";
 
+const BACKEND_URL: any  = process.env.NEXT_PUBLIC_BACKEND_URL;
+
 export async function GET() {
-    const res = await fetch("https://my-json-server.typicode.com/vicenteh2o/json-server/todos");
+    const res = await fetch(BACKEND_URL);
     const todos = await res.json();
 
     return NextResponse.json(todos)
+}
+
+export async function POST(request: any) {
+    const body = await request.json();
+
+    const res = await fetch(BACKEND_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    })
+    const todo = await res.json();
+
+    return NextResponse.json(todo);
 }
--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -5,9 +5,34 @@ async function fetchData() {
     return response.json();
 }
 
-const Page = async () => {
-    const data = await fetchData();
+interface NewTodo {
+    id: string;
+    title: string;
+    completed: boolean;
+}
 
+async function postData(newTodo: NewTodo) {
+    const response = await fetch("http://localhost:3000/api/todoItem", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newTodo)
+    })
+    return await response.json();
+}
+
+const Page = async () => {
+    const newTodo = {
+        id: "4",
+        title: "New todo testing",
+        completed: false
+    }
+
+    const createdTodo = await postData(newTodo);
+    console.log("Created todo: ", createdTodo);
+
+    const data = await fetchData();
     console.log(data)
 
     return <div>
