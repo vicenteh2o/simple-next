919b162d4603e1e12b66306f4f9ab68d4ffa2457
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-05-19 07:23:59 -0300

    feat: simple routing on next

---
+++ app/about/projects/[id]/page.tsx
@@ -0,0 +1,7 @@
+
+const ProjectView = () => {
+  return (
+    <div>project view with id</div>
+  )
+}
+export default ProjectView
---
+++ app/about/projects/page.tsx
@@ -0,0 +1,7 @@
+
+const Projects = () => {
+  return (
+    <div>My projects</div>
+  )
+}
+export default Projects
